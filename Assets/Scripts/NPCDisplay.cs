3b00c33590a182bbec3bf6936bd13b1590c13292
leesy015599 <leesy015599@gmail.com>
2023-09-07 11:24:38 +0900

    Delete unused packages

--- Assets/Scripts/NPCDisplay.cs
+++ Assets/Scripts/NPCDisplay.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCDirection : MonoBehaviour
+public class NPCDisplay : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private SpriteRenderer NPCRenderer;
@@ -15,15 +15,11 @@ public class NPCDirection : MonoBehaviour
 
     private void LookPlayer(GameObject player)
     {
-        // 기본이 왼쪽.
-        // 플레이어x값과 NPC x값의 차이가 양수일 때 flipx true
         NPCRenderer.flipX = player.transform.position.x - transform.position.x > 0;
     }
 
     private void BehindPlayer(GameObject player)
     {
-        // 기본이 플레이어보다 앞에 있을 때. Order In Layer 가 더 높다.
-        // 뒤에 있을 경우 낮게.
         NPCRenderer.sortingOrder = player.transform.position.y < transform.position.y ? 9 : 11;
     }
 }
